a16f36501ce3ed05da364ac568a9b23407b631cb
Crowdbotics <engineering@crowdbotics.com>
2023-04-15 00:09:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ancient-butterfly-d-76731",
+  url: "https://ancient-butterfly-d-76731.botics.co",
+  api: "https://ancient-butterfly-d-76731.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
